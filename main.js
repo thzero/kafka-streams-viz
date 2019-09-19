0c61649de6dfff469c2597349b39b3c1c78fcc46
thzero <thzero@users.noreply.github.com>
2019-09-18 23:41:50 -0500

    Standard graphViz output

--- main.js
+++ main.js
@@ -2,7 +2,6 @@
  * @author zz85 (https://github.com/zz85 | https://twitter.com/blurspline)
  */
 
-var dpr, rc, ctx;
 const DEBUG = false;
 const STORAGE_KEY = 'kafka-streams-viz';
 
@@ -25,14 +24,16 @@ function convertTopoToDot(topo) {
 		var match = sub.exec(line);
 
 		if (match) {
-			if (results.length) results.push(`}`);
+			if (results.length) 
+				results.push(`}`);
+				
 			results.push(`subgraph cluster_${match[1]} {
-			label = "${match[0]}";
+	label = "${match[0]}";
 
-			style=filled;
-			color=lightgrey;
-			node [style=filled,color=white];
-			`);
+	style=filled;
+	color=lightgrey;
+	node [style=filled,color=white];
+	`);
 
 			return;
 		}
@@ -75,14 +76,16 @@ function convertTopoToDot(topo) {
 			var targets = match[1];
 			targets.split(',').forEach(name => {
 				var linkedName = processName(name.trim());
-				if (linkedName === 'none') return;
+				if (linkedName === 'none') 
+					return;
 
 				results.push(`"${entityName}" -> "${linkedName}";`);
 			});
 		}
 	})
 
-	if (results.length) results.push(`}`);
+	if (results.length) 
+		results.push(`}`);
 
 	results = results.concat(outside);
 
@@ -95,47 +98,29 @@ function convertTopoToDot(topo) {
 	});
 
 	return `
-	digraph G {
-		label = "Kafka Streams Topology"
+digraph G {
+	label = "Kafka Streams Topology"
 
-		${results.join('\n')}
-	}
-	`;
+	${results.join('\n')}
+}
+`;
 }
 
 function update() {
 	var topo = input.value;
 	var dotCode = convertTopoToDot(topo);
-	if (DEBUG) console.log('dot code\n', dotCode);
-
-
-	graphviz_code.value = dotCode;
+	if (DEBUG)
+		console.log('dot code\n', dotCode);
 
 	var params = {
 		engine: 'dot',
 		format: 'svg'
   	};
-
-	var svgCode = Viz(dotCode, params);
-
-	svg_container.innerHTML = svgCode;
-
-	var svg = svg_container.querySelector('svg')
-	dpr = window.devicePixelRatio
-	canvas.width = svg.viewBox.baseVal.width * dpr | 0;
-	canvas.height = svg.viewBox.baseVal.height * dpr | 0;
-	canvas.style.width = `${svg.viewBox.baseVal.width}px`;
-	canvas.style.height = `${svg.viewBox.baseVal.height}px`;
-
-	rc = rough.canvas(canvas);
-	ctx = rc.ctx
-	ctx.scale(dpr, dpr);
-
-	var g = svg.querySelector('g');
-
+  	
 	try {
-		traverseSvgToRough(g);
-
+		graphviz_code.value = dotCode;
+		svg_container.innerHTML = Viz(dotCode, params);
+	    
 		sessionStorage.setItem(STORAGE_KEY, topo);
 	}
 	catch (e) {
@@ -144,132 +129,11 @@ function update() {
 	}
 }
 
-/**
- * The following part can be removed when rough.js adds support for rendering svgs.
- */
-
-function getFillStroke(child) {
-	var fill = child.getAttribute('fill');
-	var stroke = child.getAttribute('stroke');
-
-	return {
-		fill: fill === 'none' ? null : fill,
-		stroke: stroke === 'none' ? null : stroke
-	};
-}
-
-function splitToArgs(array, delimiter) {
-	return array.split(delimiter || ',').map(v => +v);
-}
-
-// node traversal function
-function traverseSvgToRough(child) {
-
-	if (child.nodeName === 'path') {
-		var d = child.getAttribute('d');
-		var opts = getFillStroke(child);
-		rc.path(d, opts);
-		return;
-	  }
-
-	if (child.nodeName === 'ellipse') {
-		var cx = +child.getAttribute('cx');
-		var cy = +child.getAttribute('cy');
-		var rx = +child.getAttribute('rx');
-		var ry = +child.getAttribute('ry');
-
-		var opts = getFillStroke(child);
-
-		rc.ellipse(cx, cy, rx * 1.5, ry * 1.5);
-		return;
-  	}
-
-	if (child.nodeName === 'text') {
-		var fontFamily = child.getAttribute('font-family')
-		var fontSize = +child.getAttribute('font-size')
-		var anchor = child.getAttribute('text-anchor')
-
-		if (anchor === 'middle') {
-			ctx.textAlign = 'center';
-		}
-
-		if (fontFamily) {
-			ctx.fontFamily = fontFamily;
-		}
-
-		if (fontSize) {
-			ctx.fontSize = fontSize;
-		}
-
-		ctx.fillText(child.textContent, child.getAttribute('x'), child.getAttribute('y'));
-		return;
-  	}
-
-	if (child.nodeName === 'polygon') {
-		var pts = child.getAttribute('points')
-
-		var opts = getFillStroke(child);
-		rc.path(`M${pts}Z`, opts);
-
-		return;
-	}
-
-	if (child.nodeName === 'g') {
-		var transform = child.getAttribute('transform');
-		ctx.save();
-
-		if (transform) {
-	  		var scale = /scale\((.*)\)/.exec(transform);
-	  		if (scale) {
-				var args = scale[1].split(' ').map(parseFloat);
-				ctx.scale(...args);
-	  		}
-
-			var rotate = /rotate\((.*)\)/.exec(transform);
-			if (rotate) {
-				var args = rotate[1].split(' ').map(parseFloat);
-				ctx.rotate(...args);
-			}
-
-			var translate = /translate\((.*)\)/.exec(transform);
-			if (translate) {
-				var args = translate[1].split(' ').map(parseFloat);
-				ctx.translate(...args);
-			}
-		}
-
-		[...child.children].forEach(traverseSvgToRough);
-
-		ctx.restore();
-		return;
-	}
-}
-
-var pending;
-function scheduleUpdate() {
-	if (pending) clearTimeout(pending);
-
-	pending = setTimeout(() => {
-		pending = null;
-		update();
-	}, 200);
-}
-
 // startup
 var topo;
-
-if (window.location.hash.length > 1) {
-	try {
-		topo = atob(window.location.hash.substr(1));
-	} catch {
-		console.log("Can not read topo from url hash");
-		window.location.hash = "";
-	}
-}
-
-if (!topo) {
+if (!topo)
 	topo = sessionStorage.getItem(STORAGE_KEY);
-}
 
-if (topo) input.value = topo;
+if (topo) 
+	input.value = topo;
 update();
